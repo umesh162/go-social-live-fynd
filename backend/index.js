000a1c0dfa8eaa222ebee2746a50a91d3aa5485e
Umesh palival <umesh.paliva@aiab.in>
2022-09-04 01:37:10 +0530

    Deploying to heroku

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
@@ -20,6 +21,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use("/", routes());
+
+// --------deploying Application -----------
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const port = process.env.PORT || 8000;
 
